6116d5617afdff2be3797f37d5367d9d8f8cf6a3
darriola1 <denn.arr17@gmail.com>
2024-11-24 14:44:51 -0300

    correcciones minimas

--- src/controllers/balanceController.js
+++ src/controllers/balanceController.js
@@ -5,19 +5,16 @@ export class BalanceController {
 
     // Método estático para crear un nuevo usuario.
     static async getBalanceById(req, res) {
-        console.log('req', req.body)
-        const { id } = req.params;
-        const usuario_id = 'denis'
-
-        console.log(id)
+        const { id: alumno_id } = req.params;
+        const { id: usuario_id } = req.user
 
         try {
             // Hasheamos la contraseña antes de guardar el usuario
-            const balance = await BalanceModel.getBalanceById(id);
-            logger.info(`El usuario ${usuario_id} ha consultado el balance de ${id}`);
+            const balance = await BalanceModel.getBalanceById(alumno_id);
+            logger.info(`El usuario ${usuario_id} ha consultado el balance de ${alumno_id}`);
             return res.status(200).json(balance);
         } catch (error) {
-            logger.error(`Error consultado balance de ${id} por usuario ${usuario_id}`);
+            logger.error(`Error consultado balance de ${alumno_id} por usuario ${usuario_id}`);
             res.status(500).json({ error: 'Error al obtener balance' });
         }
     }
